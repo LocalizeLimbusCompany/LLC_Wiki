f506ddc4e8cf457d2bad51cc1f126cce142680a8
ZengXiaoPi <1768506869@qq.com>
2024-10-04 19:44:29 +0800

    2024-10-04 19:44:29 - Auto Build Script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "ZengXiaoPi", // Usually your GitHub org/user name.
-  projectName: "zero-doc", // Usually your repo name.
+  organizationName: "LocalizeLimbusCompany", // Usually your GitHub org/user name.
+  projectName: "LLC_Wiki", // Usually your repo name.
+  deploymentBranch: "main",
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
